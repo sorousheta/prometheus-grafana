86803587f1a8751c6943b807932bc62d995dcc73
sorousheta <64336292+sorousheta@users.noreply.github.com>
2024-09-02 04:00:21 +0330

    Deploy

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# prometheus-grafana
- 
+# Grafana-Mikrotik
+
+![logo](https://repository-images.githubusercontent.com/366494855/c62052b8-17c2-47f2-a3ae-0e397a3ef074)
+
+![visitors](https://visitor-badge.laobi.icu/badge?page_id=IgorKha.Grafana-Mikrotik)
+![example branch parameter](https://github.com/IgorKha/Grafana-Mikrotik/actions/workflows/action.yml/badge.svg?branch=master)
+![mikrotikOS](https://img.shields.io/badge/Mikrotik_ROS-v7.4-blue)
+![Grafana](https://img.shields.io/badge/Grafana-v9.0.5-orange?logo=grafana)
+![Prometheus](https://img.shields.io/badge/Prometheus-v2.37.0-red?logo=prometheus)
+![snmp_exporter](https://img.shields.io/badge/snmp__exporter-v0.20.0-red?logo=prometheus)
+
+
+---
+
+## 🐳 Deploy with docker-compose
+
+### Deploy with bash script
+
+```console
+git clone https://github.com/sorousheta/prometheus-grafana.git
+cd ./prometheus-grafana
+bash ./run.sh --config
+```
+
+```console
+  You can also pass some arguments to script to set some these options:
+
+    --config: change the user and password to grafana and specify the mikrotik IP address
+
+    --stop: stop docker containers
+
+    --help
+```
+
+For example:
+
+```console
+    bash run.sh --config
+```
+
+
+### deploy with docker-compose manual
+
+1.Change targets ip (192.168.88.1) into file prometheus/prometheus.yml
+
+2.Run
+
+```console
+docker-compose up -d
+```
+
+3.Open [localhost:3000](http://localhost:3000)
+
+* Grafana login: `admin`
+
+* Password: `mikrotik`
+
+If you want to change the credentials, then edit the ".env" file
+
+---
+
+## Manual deploy
+
+1.add into prometheus.yml
+
+```yml
+  - job_name: Mikrotik
+    static_configs:
+      - targets:
+        - 192.168.88.1  # SNMP device IP.
+    metrics_path: /snmp
+    params:
+      module: [mikrotik]
+    relabel_configs:
+      - source_labels: [__address__]
+        target_label: __param_target
+      - source_labels: [__param_target]
+        target_label: instance
+      - target_label: __address__
+        replacement: localhost:9116  # The SNMP exporter's real hostname:port.
+```
+
+2.Configure Prometheus and run /snmp/snmp_exporter
+
+3.Add dashboard <https://grafana.com/grafana/dashboards/14420>
+
+---
+
+### Docker snmp_exporter (deprecated)
+
+[![Docker Pulls](https://img.shields.io/docker/pulls/mashinkopochinko/snmp_exporter_mikrotik?logo=docker)](https://hub.docker.com/repository/docker/mashinkopochinko/snmp_exporter_mikrotik)
+
+> amd64-linux container
+
+```console
+sudo docker run -d -p 9116:9116 mashinkopochinko/snmp_exporter_mikrotik:latest
+```
+
+---
+![img1](/readme/screen.png)
---
+++ run.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+############################################################################
+#   You can also pass some arguments to script to set some these options:
+#   
+#       --config: change the user and password to grafana and specify the mikrotik IP address
+#       --stop: stop docker containers
+#   
+#   For example:
+#       bash run.sh --config
+#   
+############################################################################
+
+set -e
+
+REPO=Grafana-Mikrotik
+ENV_FILE=${ENV_FILE:-.env}
+ENV_FILE_GF=${ENV_FILE_GF:-.grafana}
+ENV_FILE_PROMETHEUS=${ENV_FILE_PROMETHEU:-.prometheus}
+
+#? Colors
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+BOLD='\033[1m'
+RESET='\033[m' #? No Color
+
+BOLD='\033[1m'
+
+ask() {
+    local prompt default reply
+
+    if [[ ${2} = 'Y' ]]; then
+        prompt='Y/n'
+        default='Y'
+    elif [[ ${2} = 'N' ]]; then
+        prompt='y/N'
+        default='N'
+    else
+        prompt='y/n'
+        default=''
+    fi
+
+    while true; do
+
+        #? Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [${prompt}] "
+
+        #? Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read -r reply </dev/tty
+
+        #? Default?
+        if [[ -z ${reply} ]]; then
+            reply=${default}
+        fi
+
+        #? Check if the reply is valid
+        case "${reply}" in
+        Y* | y*) return 0 ;;
+        N* | n*) return 1 ;;
+        esac
+    done
+}
+
+command_exists() {
+    command -v "$@" >/dev/null 2>&1 || {
+        fmt_error "$* is not installed. Please install $* first."
+        exit 1
+    }
+}
+
+fmt_error() {
+    echo -e "\n${BOLD}${RED}Error: $*${RESET}\n" >&2
+}
+
+help() {
+    if [ "${HELP}" = yes ]; then
+        sed -n -e 's/^#   //' -e '3,12p;' "$0"
+        exit 1
+        return
+    fi
+}
+
+clone_git() {
+
+    echo -e "${BLUE}Git cloning ${REPO}...${RESET}"
+    git clone --depth=1 https://github.com/IgorKha/${REPO}.git ||
+        {
+            fmt_error "git clone of ${REPO} repo failed"
+            exit 1
+        }
+
+    echo
+}
+
+router_ip() {
+    if [[ "${CONFIG}" = yes ]]; then
+        IP=$(grep -R 'MIKROTIK_IP' "${ENV_FILE_PROMETHEUS}" 2>&1 | cut -d= -f2)
+        echo -e "\n${BLUE}===================================="
+        echo -e "\n${BOLD}Prometheus${RESET}\n"
+        if ask "Change target mikrotik IP address ? (current ${IP})" Y; then
+            read -rp 'Enter target mikrotik IP address: ' IP
+            if [ -d "./${REPO}" ]; then
+                sed -ri -e '/mikrotik_ip/s/(- ).*( #.*)/\1'"${IP}"'\2/g' \
+                    ${REPO}/prometheus/prometheus.yml
+                sed -ri -e 's/^(MIKROTIK_IP=)(.*)$/\1'"$IP"'/g' "${ENV_FILE_PROMETHEUS}"
+                echo -e "\n${GREEN}... Prometheus target IP changed to ${IP}"
+            else
+                sed -ri -e '/mikrotik_ip/s/(- ).*( #.*)/\1'"${IP}"'\2/g' \
+                    ./prometheus/prometheus.yml
+                sed -ri -e 's/^(MIKROTIK_IP=)(.*)$/\1'"${IP}"'/g' "${ENV_FILE_PROMETHEUS}"
+                echo -e "\n${GREEN}... Prometheus target IP changed to ${IP}"
+            fi
+            return
+        fi
+        echo -e "\n${BLUE}...Skipped step"
+        return
+    fi
+}
+
+# snmp_on() {
+#     if [ "$CONFIG" = yes ]; then
+#         echo -e "\n${BLUE}===================================="
+#         echo -e "${BOLD}Mikrotik SNMP ACTIVATION${RESET}"
+#         if ask "Activate snmp mikrotik using ssh?" N; then
+#             read -rp 'Enter login: ' MK_LOGIN
+#             # read -rsp 'Enter password: ' MK_PASSWD
+
+#             COMM="
+#             snmp
+#             "
+#             ssh ${MK_LOGIN}@$IP "${COMM}"
+
+#         else
+#             echo "skipped"
+#         fi
+#         return
+#     fi
+# }
+
+grafana_credentials() {
+    if [[ "${CONFIG}" = yes ]]; then
+        echo -e "\n${YELLOW}===================================="
+        echo -e "\n${BOLD}Grafana${RESET}\n"
+        if ask "Change default credentials Grafana ?" N; then
+            read -rp 'Enter grafana Username: ' GF_USER
+            read -rsp 'Enter grafana Password: ' GF_PASSWD
+
+            sed -ri -e 's/^(GF_SECURITY_ADMIN_USER=)(.*)$/\1'"${GF_USER}"'/g' "${ENV_FILE_GF}"
+            sed -ri -e 's/^(GF_SECURITY_ADMIN_PASSWORD=)(.*)$/\1'"${GF_PASSWD}"'/g' "${ENV_FILE_GF}"
+        else
+            echo -e "Default Grafana:
+            User: ${YELLOW}admin${RESET}
+            Password: ${YELLOW}mikrotik${RESET}"
+        fi
+        return
+    fi
+}
+
+docker-cmd() {
+    if [[ "${STOP}" = yes ]]; then
+        if [ -d "./${REPO}" ]; then
+            cd ${REPO} && docker compose down
+        else
+            docker compose down
+        fi
+    else
+        if [[ -d "./${REPO}" ]]; then
+            cd ${REPO} && docker compose up -d
+            print_success
+        else
+            docker compose up -d
+            print_success
+        fi
+    fi
+}
+
+print_success() {
+    echo "============================================="
+    echo -e "${GREEN}Grafana http://localhost:3000"
+    echo -e "Prometheus http://localhost:9090/targets${RESET}"
+}
+
+main() {
+
+    #? Parse arguments
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        --help) HELP=yes ;;
+        --config) CONFIG=yes ;;
+        --stop) STOP=yes ;;
+        esac
+        shift
+    done
+
+    help
+
+    command_exists git
+    command_exists docker
+
+    #? init
+    if [[ -d "./${REPO}" ]]; then
+        ENV_FILE=${REPO}/.env
+        ENV_FILE_GF=${REPO}/.grafana
+        ENV_FILE_PROMETHEUS=${REPO}/.prometheus
+    elif [[ ! -e ${ENV_FILE} ]]; then
+        clone_git
+        ENV_FILE=${REPO}/.env
+        ENV_FILE_GF=${REPO}/.grafana
+        ENV_FILE_PROMETHEUS=${REPO}/.prometheus
+    fi
+
+    router_ip
+    # snmp_on
+    grafana_credentials
+
+    # Change UID:GID prometheus container to current user
+    sed -ri -e 's/^(CURRENT_USER=)(.*)$/\1'"$(id -u)\:$(id -g)"'/g' "${ENV_FILE}"
+
+    docker-cmd
+}
+
+main "$@"
